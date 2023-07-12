8abd026974babfa1aafaf613d041301e5262d6e9
Valentin Likholat <phantom.231220@gmail.com>
2023-07-13 02:58:34 +0300

    Completed 3 Task

--- Program.cs
+++ Program.cs
@@ -44,6 +44,7 @@ Print2DArray(myArray);
 //Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+/*
 int InputNum(string message) //Ввод с клавиатуры
 {
     Console.Write(message);
@@ -93,3 +94,73 @@ Print2DArray(myArray);
 int pos1 = InputNum("Введите позицию в строке: ");
 int pos2 = InputNum("Введите позицию в столбце: ");
 Location(myArray, pos1, pos2);
+*/
+
+
+//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+int InputNum(string message) //Ввод с клавиатуры
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int cols) //Создание двумерного массива
+{
+    return new int[rows, cols];
+}
+
+void Fill2DArray(int[,] array) //Заполнение двумерного массива
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = rnd.Next(-100, 101);
+}
+
+void Print2DArray(int[,] array) //Вывод двумерного массива на экран
+{
+    Console.Write("Ваш массив: \n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+double[] AverageColumns(int[,] array) //Вычисление среднего арифметического в массиве столбцов
+{
+    double[] avgColumns = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            avgColumns[j] += Convert.ToDouble(array[i, j]);
+    }
+    for (int i = 0; i < avgColumns.Length; i++)
+        avgColumns[i] = Math.Round(avgColumns[i] / array.GetLength(0), 2);
+    return avgColumns;
+}
+
+void PrintArray(double[] array) //Вывод массива на экран
+{
+    //Console.Write("Ваш массив: ");
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(" ");
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+
+int[,] my2DArray = Create2DArray(rows, columns);
+Fill2DArray(my2DArray);
+Print2DArray(my2DArray);
+
+Console.WriteLine("Среднее арифметическое столбцов:");
+double[] myArray = AverageColumns(my2DArray);
+PrintArray(myArray);
